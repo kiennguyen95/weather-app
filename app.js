8ddd5bb2cbe1e0b8e27c7a263b1318d42424adec
Kien Nguyen <kien.nguyentrung.95@gmail.com>
2021-02-26 16:10:19 +0700

    Refactor by using ES6 syntax

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ if (!address) {
     return console.log('Please provide an address')
 }
 
-geocode(address, (error, data) => {
+geocode(address, (error, { latitude, longitude, location } = {}) => {
     if (error) {
         return console.log(error)
     }
 
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+    forecast(latitude, longitude, (error, forecastData) => {
         if (error) {
             return console.log(error)
         }
 
-        console.log(data.location)
+        console.log(location)
         console.log(forecastData)
     })
 })
